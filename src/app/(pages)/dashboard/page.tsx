c65c48fbff987997f649f3ca9444ad1b2ac623e1
Vilela <guilhermevilela@hubert.com.br>
2025-01-15 16:59:11 -0300

    fix: page-name

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { useAuthContext } from "@/context/authContext";
 import { useDashboardController } from "@/hooks/dashboard";
 import { ArrowsRightLeftIcon, PlusIcon } from "@heroicons/react/16/solid";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Dashboard() {
@@ -30,62 +31,68 @@ export default function Dashboard() {
   const { user } = useAuthContext();
 
   return (
-    <div className=" flex flex-col gap-3 max-w-7xl mx-auto p-8 h-screen">
-      <div className="flex items-center justify-between">
-        <Link href="/dashboard" className="mb-4">
-          <img src="/images/logo.svg" />
-        </Link>
+    <>
+      <Head>
+        <title>Bolt | Dashboard </title>
+      </Head>
 
-        <Button className="max-w-32 w-32" onClick={logout}>
-          Sair
-        </Button>
-      </div>
+      <div className=" flex flex-col gap-3 max-w-7xl mx-auto p-8 h-screen">
+        <div className="flex items-center justify-between">
+          <Link href="/dashboard" className="mb-4">
+            <img src="/images/logo.svg" />
+          </Link>
 
-      <header className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-gray-900">
-          Dashboard Financeira de {user?.username}
-        </h1>
+          <Button className="max-w-32 w-32" onClick={logout}>
+            Sair
+          </Button>
+        </div>
 
-        <Wallet value={balance} />
-      </header>
+        <header className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-gray-900">
+            Dashboard Financeira de {user?.username}
+          </h1>
 
-      <div className="flex flex-col gap-3 rounded-xl border border-gray-200 bg-white shadow-sm">
-        <header className="p-6 pb-2">
-          <h3 className=" text-xl font-semibold leading-none tracking-tight  flex items-center gap-2 text-gray-800">
-            <div className="text-background-primary">
-              {activeOperation === "deposit" ? (
-                <PlusIcon className="size-6" />
-              ) : (
-                <ArrowsRightLeftIcon className="size-6" />
-              )}
-            </div>
-            {activeOperation === "deposit"
-              ? "Realizar Depósito"
-              : "Realizar Transferência"}
-          </h3>
+          <Wallet value={balance} />
         </header>
 
-        <CardTransaction
-          activeOperation={activeOperation}
-          amount={amount}
-          recipientId={recipientId}
-          onActiveOperation={handleSetActiveOperation}
-          onSetAmount={handleSetAmount}
-          onSetRecipientId={handleSetRecipientId}
-          onSubmit={() =>
-            activeOperation === "deposit" ? handleDeposit() : handleTransfer()
-          }
-          errors={errors}
-        />
+        <div className="flex flex-col gap-3 rounded-xl border border-gray-200 bg-white shadow-sm">
+          <header className="p-6 pb-2">
+            <h3 className=" text-xl font-semibold leading-none tracking-tight  flex items-center gap-2 text-gray-800">
+              <div className="text-background-primary">
+                {activeOperation === "deposit" ? (
+                  <PlusIcon className="size-6" />
+                ) : (
+                  <ArrowsRightLeftIcon className="size-6" />
+                )}
+              </div>
+              {activeOperation === "deposit"
+                ? "Realizar Depósito"
+                : "Realizar Transferência"}
+            </h3>
+          </header>
 
-        <hr />
+          <CardTransaction
+            activeOperation={activeOperation}
+            amount={amount}
+            recipientId={recipientId}
+            onActiveOperation={handleSetActiveOperation}
+            onSetAmount={handleSetAmount}
+            onSetRecipientId={handleSetRecipientId}
+            onSubmit={() =>
+              activeOperation === "deposit" ? handleDeposit() : handleTransfer()
+            }
+            errors={errors}
+          />
 
-        <CardOperations
-          operationHistory={operationHistory}
-          onRevertOperation={handleRevert}
-          loading={loading}
-        />
+          <hr />
+
+          <CardOperations
+            operationHistory={operationHistory}
+            onRevertOperation={handleRevert}
+            loading={loading}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,6 +2,7 @@
 import { Login } from "@/components/login";
 import { Register } from "@/components/register";
 import { useHomeController } from "@/hooks/login";
+import Head from "next/head";
 
 export default function Home() {
   const {
@@ -20,34 +21,40 @@ export default function Home() {
   } = useHomeController();
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-blue-800 to-cyan-400">
-      {isRegister ? (
-        <Register
-          username={username}
-          password={password}
-          email={email}
-          error={error}
-          loading={loading}
-          onSetUsername={handleSetUsername}
-          onSetPassword={handleSetPassword}
-          onSetEmail={handleSetEmail}
-          onSubmit={handleSubmit}
-          onSetRegister={() => {
-            handleSetIsRegister(false), onClearFields();
-          }}
-        />
-      ) : (
-        <Login
-          username={username}
-          password={password}
-          error={error}
-          loading={loading}
-          onSetUsername={handleSetUsername}
-          onSetPassword={handleSetPassword}
-          onSubmit={handleSubmit}
-          onSetRegister={() => handleSetIsRegister(true)}
-        />
-      )}
-    </div>
+    <>
+      <Head>
+        <title>Bolt | Login </title>
+      </Head>
+
+      <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-blue-800 to-cyan-400">
+        {isRegister ? (
+          <Register
+            username={username}
+            password={password}
+            email={email}
+            error={error}
+            loading={loading}
+            onSetUsername={handleSetUsername}
+            onSetPassword={handleSetPassword}
+            onSetEmail={handleSetEmail}
+            onSubmit={handleSubmit}
+            onSetRegister={() => {
+              handleSetIsRegister(false), onClearFields();
+            }}
+          />
+        ) : (
+          <Login
+            username={username}
+            password={password}
+            error={error}
+            loading={loading}
+            onSetUsername={handleSetUsername}
+            onSetPassword={handleSetPassword}
+            onSubmit={handleSubmit}
+            onSetRegister={() => handleSetIsRegister(true)}
+          />
+        )}
+      </div>
+    </>
   );
 }
